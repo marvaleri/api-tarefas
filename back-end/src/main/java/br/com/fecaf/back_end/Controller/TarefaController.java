112412b6455b2fb33749f07b31000e133d929dd9
Marlon <marlonsoares154@gmail.com>
2025-07-11 23:52:07 -0300

    feat: new function Listar

--- back-end/src/main/java/br/com/fecaf/back_end/Controller/TarefaController.java
+++ back-end/src/main/java/br/com/fecaf/back_end/Controller/TarefaController.java
@@ -1,4 +1,24 @@
 package br.com.fecaf.back_end.Controller;
 
+import br.com.fecaf.back_end.Model.Tarefa;
+import br.com.fecaf.back_end.Services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tarefas")
+@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
 public class TarefaController {
+
+    @Autowired
+    private TarefaService tarefaService;
+
+    @GetMapping("/listarTarefas")
+    public List<Tarefa> listarTarefas() { return tarefaService.listarTarefas(); }
+
 }
--- back-end/src/main/java/br/com/fecaf/back_end/Services/TarefaService.java
+++ back-end/src/main/java/br/com/fecaf/back_end/Services/TarefaService.java
@@ -1,4 +1,20 @@
 package br.com.fecaf.back_end.Services;
 
+import br.com.fecaf.back_end.Model.Tarefa;
+import br.com.fecaf.back_end.Repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    //Listar
+    public List<Tarefa> listarTarefas() { return tarefaRepository.findAll(); }
+
+
 }
